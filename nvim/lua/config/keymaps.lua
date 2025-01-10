e255b6b48e2e844af18b67635ddfb11039d0ef32
tyanxie <494966054@qq.com>
2025-01-11 00:49:29 +0800

    feat(neovim): 完善快捷键配置
    1. 增加J与K配置，方便快速向上和向下。
    2. LSP配置移除K增加U，替代原有K的Hover能力。
    3. LSP配置增加gi，代表Goto Implementation，作为gI的简便形式使用。

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,10 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- set alt-bs to delete word
-vim.keymap.set("i", "<M-BS>", "<C-w>", { noremap = true })
+-- 设置 Alt(Option) - Backspace 为删除单个单词的快捷键
+vim.keymap.set("i", "<M-BS>", "<C-w>", { desc = "Delete Word", noremap = true })
 
--- disable A-j and A-k to avoid switch line avoid by tmux in esc-j and esc-k
+-- 在tmux中使用neovim会导致ESC键被映射为 Alt 键
+-- 最终导致在快速输入的情况下很容易触发 Alt-j 和 Alt-k 快捷键，最终导致意外的交换上下两行的操作
+-- 因此这里直接删除Alt-j和Alt-k快捷键，防止此类问题的发生
 -- https://github.com/LunarVim/LunarVim/issues/1857#issuecomment-2273066106
 vim.keymap.del({ "n", "i", "v" }, "<A-j>")
 vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+
+-- 设置 J 和 K 为快速上下，一次相当于执行多次 j/k 命令
+vim.keymap.set({ "n", "x" }, "J", "5j", { desc = "Fast Down", silent = true })
+vim.keymap.set({ "n", "x" }, "K", "5k", { desc = "Fast Up", silent = true })
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,27 @@
+return {
+    "neovim/nvim-lspconfig",
+    opts = function()
+        -- 配置全局快捷键
+        local keys = require("lazyvim.plugins.lsp.keymaps").get()
+        vim.list_extend(keys, {
+            -- 移除默认的 K 代表 Hover 的能力，在 keymaps.lua 中提供其它键位用于 Hover
+            { "K", false },
+            -- 设置 gi 为 Goto Implementation 减少 gI 的使用
+            -- 该配置使用Fzf-lua实现了模糊查询相关的实现
+            -- 参考配置：https://github.com/LazyVim/LazyVim/blob/d1529f650fdd89cb620258bdeca5ed7b558420c7/lua/lazyvim/plugins/extras/editor/fzf.lua#L294
+            {
+                "gi",
+                "<cmd>FzfLua lsp_implementations jump_to_single_result=true ignore_current_line=true<cr>",
+                desc = "Goto Implementation",
+            },
+            -- 设置 U 命令为 Hover，替代默认的 K 的工作
+            {
+                "U",
+                function()
+                    vim.lsp.buf.hover()
+                end,
+                desc = "Hover",
+            },
+        })
+    end,
+}
