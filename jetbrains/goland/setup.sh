c9346740c801edaf0b6d648390018ff4fa600e3d
tyanxie <494966054@qq.com>
2025-01-10 22:31:37 +0800

    feat: 重整目录结构

--- jetbrains/goland/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "start kill GoLand app"
-ps -ef | grep /Applications/GoLand.app/Contents/MacOS/goland | grep -v grep | awk '{print $2}' | xargs kill
-echo "kill GoLand app complete"
-
-echo "start install plugins"
-open -na "GoLand.app" --args installPlugins IdeaVIM IdeaVimExtension AceJump eu.theblob42.idea.whichkey
-echo "install plugins complete"
