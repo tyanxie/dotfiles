28f431d11d04e005a499ca2dedbe46795da5a10b
tyanxie <494966054@qq.com>
2025-01-12 13:48:48 +0800

    feat(neovim): 增加toml支持

---
+++ nvim/lua/plugins/toml.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        recommended = function()
+            return LazyVim.extras.wants({
+                ft = "toml",
+                root = "*.toml",
+            })
+        end,
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                taplo = {},
+            },
+        },
+    },
+}
