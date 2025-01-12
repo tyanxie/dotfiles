9bdea5ac300d528288de4ecf9650fc6bd2ae8a63
tyanxie <494966054@qq.com>
2025-01-12 20:41:40 +0800

    feat(neovim): 完善go语言test与debug工具

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,4 +1,11 @@
 return {
+    recommended = function()
+        return LazyVim.extras.wants({
+            ft = { "go", "gomod", "gowork", "gotmpl" },
+            root = { "go.work", "go.mod" },
+        })
+    end,
+
     {
         "nvim-treesitter/nvim-treesitter",
         opts = { ensure_installed = { "go", "gomod", "gowork", "gosum", "gotmpl" } },
@@ -59,14 +66,16 @@ return {
                     LazyVim.lsp.on_attach(function(client, _)
                         if not client.server_capabilities.semanticTokensProvider then
                             local semantic = client.config.capabilities.textDocument.semanticTokens
-                            client.server_capabilities.semanticTokensProvider = {
-                                full = true,
-                                legend = {
-                                    tokenTypes = semantic.tokenTypes,
-                                    tokenModifiers = semantic.tokenModifiers,
-                                },
-                                range = true,
-                            }
+                            if semantic then
+                                client.server_capabilities.semanticTokensProvider = {
+                                    full = true,
+                                    legend = {
+                                        tokenTypes = semantic.tokenTypes,
+                                        tokenModifiers = semantic.tokenModifiers,
+                                    },
+                                    range = true,
+                                }
+                            end
                         end
                     end, "gopls")
                     -- end workaround
@@ -75,11 +84,26 @@ return {
         },
     },
 
+    -- conform 代码格式化
     {
         "stevearc/conform.nvim",
         opts = {
             formatters_by_ft = {
                 go = { "goimports", lsp_format = "last" },
+            },
+        },
+    },
+
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            {
+                "williamboman/mason.nvim",
+                opts = { ensure_installed = { "delve" } },
+            },
+            {
+                "leoluz/nvim-dap-go",
+                opts = {},
             },
         },
     },
@@ -108,123 +132,7 @@ return {
     },
 
     {
-        "mfussenegger/nvim-dap",
-        recommended = true,
-        desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
-
-        dependencies = {
-            "rcarriga/nvim-dap-ui",
-            -- virtual text for the debugger
-            {
-                "theHamsta/nvim-dap-virtual-text",
-                opts = {},
-            },
-        },
-
-        -- stylua: ignore
-        keys = {
-            { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-            { "<leader>db", function() require("dap").toggle_breakpoint() end,                                    desc = "Toggle Breakpoint" },
-            { "<leader>dc", function() require("dap").continue() end,                                             desc = "Run/Continue" },
-            { "<leader>da", function() require("dap").continue({ before = get_args }) end,                        desc = "Run with Args" },
-            { "<leader>dC", function() require("dap").run_to_cursor() end,                                        desc = "Run to Cursor" },
-            { "<leader>dg", function() require("dap").goto_() end,                                                desc = "Go to Line (No Execute)" },
-            { "<leader>di", function() require("dap").step_into() end,                                            desc = "Step Into" },
-            { "<leader>dj", function() require("dap").down() end,                                                 desc = "Down" },
-            { "<leader>dk", function() require("dap").up() end,                                                   desc = "Up" },
-            { "<leader>dl", function() require("dap").run_last() end,                                             desc = "Run Last" },
-            { "<leader>do", function() require("dap").step_out() end,                                             desc = "Step Out" },
-            { "<leader>dO", function() require("dap").step_over() end,                                            desc = "Step Over" },
-            { "<leader>dP", function() require("dap").pause() end,                                                desc = "Pause" },
-            { "<leader>dr", function() require("dap").repl.toggle() end,                                          desc = "Toggle REPL" },
-            { "<leader>ds", function() require("dap").session() end,                                              desc = "Session" },
-            { "<leader>dt", function() require("dap").terminate() end,                                            desc = "Terminate" },
-            { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                     desc = "Widgets" },
-        },
-
-        config = function()
-            -- load mason-nvim-dap here, after all adapters have been setup
-            if LazyVim.has("mason-nvim-dap.nvim") then
-                require("mason-nvim-dap").setup(LazyVim.opts("mason-nvim-dap.nvim"))
-            end
-
-            vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-
-            for name, sign in pairs(LazyVim.config.icons.dap) do
-                sign = type(sign) == "table" and sign or { sign }
-                vim.fn.sign_define(
-                    "Dap" .. name,
-                    { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-                )
-            end
-
-            -- setup dap config by VsCode launch.json file
-            local vscode = require("dap.ext.vscode")
-            local json = require("plenary.json")
-            vscode.json_decode = function(str)
-                return vim.json.decode(json.json_strip_comments(str))
-            end
-        end,
-    },
-
-    {
-        "rcarriga/nvim-dap-ui",
-        -- virtual text for the debugger
-        {
-            "theHamsta/nvim-dap-virtual-text",
-            opts = {},
-        },
-        dependencies = { "nvim-neotest/nvim-nio" },
-        -- stylua: ignore
-        keys = {
-            { "<leader>du", function() require("dapui").toggle({}) end,  desc = "Dap UI" },
-            { "<leader>de", function() require("dapui").eval() end,      desc = "Eval",  mode = { "n", "v" } },
-        },
-        opts = {},
-        config = function(_, opts)
-            local dap = require("dap")
-            local dapui = require("dapui")
-            dapui.setup(opts)
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open({})
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close({})
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close({})
-            end
-        end,
-    },
-
-    {
-        "leoluz/nvim-dap-go",
-        opts = {},
-    },
-
-    {
-        "theHamsta/nvim-dap-virtual-text",
-        opts = {},
-    },
-
-    {
-        "ray-x/go.nvim",
-        dependencies = { -- optional packages
-            "ray-x/guihua.lua",
-            "neovim/nvim-lspconfig",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        config = function()
-            require("go").setup()
-        end,
-        event = { "CmdlineEnter" },
-        ft = { "go", "gomod" },
-        build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-    },
-
-    {
         "nvim-neotest/neotest",
-        optional = true,
         dependencies = {
             "fredrikaverpil/neotest-golang",
         },
@@ -240,31 +148,18 @@ return {
     },
 
     {
-        "fredrikaverpil/neotest-golang",
-    },
-
-    {
-        "nvim-neotest/nvim-nio",
-    },
-
-    {
-        "nvimtools/none-ls.nvim",
-        optional = true,
+        "ray-x/go.nvim",
         dependencies = {
-            {
-                "williamboman/mason.nvim",
-                opts = { ensure_installed = { "gomodifytags", "impl" } },
-            },
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
         },
-        opts = function(_, opts)
-            local nls = require("null-ls")
-            opts.sources = vim.list_extend(opts.sources or {}, {
-                nls.builtins.code_actions.gomodifytags,
-                nls.builtins.code_actions.impl,
-                nls.builtins.formatting.gofumpt,
-                nls.builtins.formatting.goimports,
-            })
+        config = function()
+            require("go").setup()
         end,
+        event = { "CmdlineEnter" },
+        ft = { "go", "gomod" },
+        build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     },
 
     {
