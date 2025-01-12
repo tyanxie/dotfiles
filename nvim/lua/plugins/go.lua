4152ff66c9871c6a2dd1594a859f7e9c034c9920
tyanxie <494966054@qq.com>
2025-01-12 13:48:35 +0800

    feat(neovim): go语言增加gotmpl语法高亮支持

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        opts = { ensure_installed = { "go", "gomod", "gowork", "gosum" } },
+        opts = { ensure_installed = { "go", "gomod", "gowork", "gosum", "gotmpl" } },
     },
 
     {
