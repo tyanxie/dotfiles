8d71718b05869f51498dc85ab8f0c02062dae77a
tyanxie <494966054@qq.com>
2025-01-13 13:55:28 +0800

    feat(neovim): Linux系统上运行关闭鼠标操作能力，解决远程服务器无法复制的问题

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,3 +7,10 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
+-- 获取操作系统名称，Linux/Darwin
+local uname = vim.uv.os_uname().sysname
+
+-- 如果是Linux操作系统，则取消鼠标能力，防止远程服务器无法选择复制内容的问题
+if uname == "Linux" then
+    vim.opt.mouse = ""
+end
