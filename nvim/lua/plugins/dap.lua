cd025509c2e6d569246eaedf0df0555e3283b3db
tyanxie <494966054@qq.com>
2025-01-14 22:00:27 +0800

    feat(neovim): debug结束和退出时不关闭dap-ui

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -82,25 +82,27 @@ return {
     {
         "rcarriga/nvim-dap-ui",
         dependencies = { "nvim-neotest/nvim-nio" },
-    -- stylua: ignore
-    keys = {
-      { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
-      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
-    },
+        -- stylua: ignore
+        keys = {
+          { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+          { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+        },
         opts = {},
         config = function(_, opts)
             local dap = require("dap")
             local dapui = require("dapui")
             dapui.setup(opts)
+            -- debug开始时打开dap-ui
             dap.listeners.after.event_initialized["dapui_config"] = function()
                 dapui.open({})
             end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close({})
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close({})
-            end
+            -- debug结束和退出时关闭dap-ui
+            -- dap.listeners.before.event_terminated["dapui_config"] = function()
+            --     dapui.close({})
+            -- end
+            -- dap.listeners.before.event_exited["dapui_config"] = function()
+            --     dapui.close({})
+            -- end
         end,
     },
 
