669ac3695e42a319769793a46ddaab0d3e94db5e
tyanxie <494966054@qq.com>
2025-01-15 19:28:48 +0800

    feat(neovim): 在第120列展示高亮提示竖线

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- TAB
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-
--- 获取操作系统名称，Linux/Darwin
-local uname = vim.uv.os_uname().sysname
-
--- 如果是Linux操作系统，则取消鼠标能力，防止远程服务器无法选择复制内容的问题
-if uname == "Linux" then
-    vim.opt.mouse = ""
-end
+-- 设置高亮竖线的颜色
+vim.cmd([[highlight ColorColumn ctermbg=NONE guibg=#323438]])
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,8 +2,24 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- disable autoformat on save
+-- TAB
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- 保存时禁止自动格式化
 vim.g.autoformat = false
 
--- set scrolloff to 16
+-- 上下滚动至少展示16行
 vim.opt.scrolloff = 16
+
+-- 获取操作系统名称，Linux/Darwin
+local uname = vim.uv.os_uname().sysname
+-- 如果是Linux操作系统，则取消鼠标能力，防止远程服务器无法选择复制内容的问题
+if uname == "Linux" then
+    vim.opt.mouse = ""
+end
+
+-- 在第120列展示高亮竖线，其颜色在init.lua中进行设置，否则无法成功
+vim.opt.colorcolumn = "120"
