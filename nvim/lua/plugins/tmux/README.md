032b311b7bda73aa9ef9554eefcf70c2ad5955fd
tyanxie <494966054@qq.com>
2025-01-17 14:44:31 +0800

    feat(neovim): 移除tmux插件，相关功能都可以通过tmux快捷键完成

--- nvim/lua/plugins/tmux/README.md
+++
@@ -1,8 +0,0 @@
-# Tmux
-
-为 Neovim 实现的 tmux 工具插件，已经默认通过文件 `lua/plugins/tmux.lua` 引入。
-
-## 使用方式
-
-- `<leader>ts`：切换 tmux 会话。
-- `<leader>tn`：创建新的 tmux 会话。
--- nvim/lua/plugins/tmux/plugin/tmux.lua
+++
@@ -1,4 +0,0 @@
-local tmux = require("tmux")
-
-vim.keymap.set("n", "<leader>ts", tmux.switch, { desc = "切换tmux会话" })
-vim.keymap.set("n", "<leader>tn", tmux.new, { desc = "创建tmux会话" })
