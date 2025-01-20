f4cb76d467707b48cfc24a4fd979f37ea87e56f2
tyanxie <494966054@qq.com>
2025-01-20 16:47:20 +0800

    feat(tmux): 新增tmux缓冲区监控脚本，用于在远程服务器上复制内容时通过OSC52协议将复制内容发送到本地机器剪切板
    实现原理：
    1. 监控tmux缓冲区列表，如果有更新会进行处理。
    2. 每次处理的时候通过who等命令获取到当前活跃的所有的ssh链接的pts句柄。
    3. 以OSC52协议格式向上述获得的pts句柄发送内容。
    4. 此时只要用户使用的终端模拟器支持OSC52协议（如wezterm），则内容可以正常出现在系统剪切板。

---
+++ tmux/tmux/tmux_buffer_monitor/tmux_buffer_monitor.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# 用于监控tmux-buffer是否有变化，如果有则通过OSC52发送数据
+
+# 获取最新一个buffer的名称
+get_last_buffer_name() {
+    tmux list-buffers -F "#{buffer_name}" | head -n 1
+}
+
+# 已启动时最新的buffer名称作为初始值
+last_buffer_name="$(get_last_buffer_name)"
+
+echo "start monitor tmux buffer, current buffer: $last_buffer_name"
+
+# 监控 tmux buffer
+while true; do
+    # 获取当前最新的buffer名称
+    current_buffer_name=$(get_last_buffer_name)
+
+    # 如果当前buffer名称和上次buffer名称不一样，则说明有新内容需要处理
+    if [[ "$current_buffer_name" != "$last_buffer_name" ]]; then
+        echo "find new buffer: $current_buffer_name"
+        # 立刻更新最新处理的buffer名称
+        last_buffer_name="$current_buffer_name"
+        # 获取最新buffer中的内容
+        content=$(tmux show-buffer -b "$current_buffer_name")
+
+        # 获取活跃的ssh链接列表如果存在则进行处理
+        ssh_sessions=$(who | grep 'pts')
+        if [ -n "$ssh_sessions" ]; then
+            # 向所有当前活跃的ssh链接中发送通过OSC52发送内容
+            echo "$ssh_sessions" | sort -u -t ' ' -k 5,5 | awk '{print $2}' | while read -r terminal; do
+                terminal="/dev/$terminal"
+                printf "\033]52;c;$(echo -n "$content" | base64)\a" >>$terminal
+                printf "send buffer [%s] content to ssh session [%s]\n" "$current_buffer_name" "$terminal"
+            done
+        fi
+    fi
+
+    # 间隔200s进行处理，复制内容尽量快速
+    sleep 0.2
+done
