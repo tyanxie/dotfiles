826e70201c3c1e447dcdf16ac238c5d6b7954ee6
tyanxie <494966054@qq.com>
2025-01-21 10:47:58 +0800

    feat(neovim): 移除formatoptions选项中的c/r/o选项，实现在注释行换行时不会自动增加注释符号

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,8 @@ require("config.lazy")
 
 -- 设置高亮竖线的颜色
 vim.cmd([[highlight ColorColumn ctermbg=NONE guibg=#323438]])
+
+-- 移除formatoptions选项中的c/r/o选项，实现在注释行换行时不会自动增加注释符号
+vim.opt.formatoptions:remove("c")
+vim.opt.formatoptions:remove("r")
+vim.opt.formatoptions:remove("o")
