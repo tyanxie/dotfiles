1685f97dde452940dd44e8b73eb7f18a9e8a0c08
tyanxie <494966054@qq.com>
2025-02-02 22:18:53 +0800

    feat(wezterm): 降低文字背景不透明度

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -26,7 +26,8 @@ config.window_background_opacity = 0.9
 -- macOS下窗口背景模糊
 config.macos_window_background_blur = 70
 -- 文字背景透明度
-config.text_background_opacity = 0.9
+-- 在Neovim中相当于colorscheme相对于wezterm背景图的不透明度
+config.text_background_opacity = 0.8
 
 -- 背景，配置参考：https://www.bilibili.com/video/BV1miWMe9Esq?vd_source=66989d6205583753c78874fada7f0721
 config.background = {
