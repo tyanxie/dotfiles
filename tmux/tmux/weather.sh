6ff5e96ba4c16247299d0c92376bf369d2356402
tyanxie <494966054@qq.com>
2025-02-06 19:31:27 +0800

    feat(tmux): weather.sh输出错误内容到文件

--- tmux/tmux/weather.sh
+++ tmux/tmux/weather.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-filename="${TMPDIR:-/tmp}tmux-weather.tmp"                   # 天气信息存放路径
+filename="${TMPDIR:-/tmp}tmux-weather.tmp"                # 天气信息存放路径
 url="https://wttr.in/Shenzhen?format=%l:%C+%t&lang=zh-cn" # 天气信息API接口
 
 # 如果文件不存在或文件超过10分钟未修改就更新其中的内容
 if [ ! -f "$filename" ] || [ "$(find "$filename" -mmin +10)" ]; then
-    # 获取天气信息
-    weather=$(curl "$url")
+    # 获取天气信息，输出结果到文件，使用&>覆盖输出并且携带错误输出，使得错误信息可以被回顾
+    curl "$url" &>"$filename"
     # 命令的执行结果
     result=$?
-    # 如果执行成功则将结果输出文件，否则输出错误信息到文件
-    if [ $result -eq 0 ]; then
-        echo "$weather" >"$filename"
-    else
-        echo "<获取天气信息失败>" >"$filename"
+    # 命令执行失败输出错误提示，使得无论如何都能展示出有效信息
+    if [ ! $result -eq 0 ]; then
+        echo "<获取天气信息失败>" >>"$filename"
     fi
 fi
 
