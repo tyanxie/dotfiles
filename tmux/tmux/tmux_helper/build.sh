b5022316b07f223554d8609252e559bb709d317a
tyanxie <494966054@qq.com>
2025-02-07 22:54:33 +0800

    feat(tmux): tmux_helper基础建设
    1. 新增build.sh脚本用于编译构建可执行文件。
    2. 新增exec.sh脚本用于在不同系统和架构上执行tmux_helper。
    3. 预编译构建darwin-arm64和linux-amd64版本的可执行文件。

---
+++ tmux/tmux/tmux_helper/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 构建脚本，编译tmux_helper
+
+# 编译时的ldflags参数
+# `-s`禁用符号表，`-w`禁用DWARF调试信息，减少包体积
+ldflags="-s -w"
+
+# 编译命令
+build_base_command="go build -ldflags=\"$ldflags\" -v"
+
+# 编译执行函数
+build() {
+    # 系统
+    local os="$1"
+    # 架构
+    local arch="$2"
+    # 构造编译命令
+    command="CGO_ENABLED=0 GOOS=$os GOARCH=$arch $build_base_command -o tmux_helper.$os.$arch"
+    # 打印命令用于提示
+    echo "$command"
+    # 执行编译命令
+    sh -c "$command"
+}
+
+# 编译darwin arm64版本
+build darwin arm64
+
+# 编译linux amd64版本
+build linux amd64
---
+++ tmux/tmux/tmux_helper/exec.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 按照操作系统执行tmux_helper的工具
+
+# 使用uname -s获取操作系统，同时通过tr命令将所有字符转为小写
+os=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+# 使用uname -m获取系统架构
+arch=$(uname -m)
+
+# 使用os和arch拼接文件路径
+filename="$HOME/.config/tmux/tmux_helper/tmux_helper.$os.$arch"
+
+# 判断文件是否存在
+if [ ! -f "$filename" ]; then
+    echo "<找不到待执行程序：$filename>"
+    exit 1
+fi
+
+# 判断文件是否可执行
+if [[ ! -x "$filename" ]]; then
+    # 尝试为文件添加可执行权限
+    if ! chmod +x "$filename"; then
+        echo "<添加可执行权限失败：$filename>"
+        exit 1
+    fi
+fi
+
+# 执行并透传所有参数
+$filename "$@"
