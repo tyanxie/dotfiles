6adbce8388251f1d4e61805c979ee7f42e9aabbd
tyanxie <494966054@qq.com>
2025-02-07 12:38:06 +0800

    feat(neovim): 完善which-key配置

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -10,6 +10,7 @@ return -- which-key helps you remember key bindings by showing a popup
             {
                 mode = { "n", "v" },
                 { "<leader><tab>", group = "tabs" },
+                { "<leader>;", icon = { icon = "" } },
                 { "<leader>m", icon = { icon = "", color = "cyan" } },
                 { "<leader>c", group = "code" },
                 { "<leader>d", group = "debug" },
