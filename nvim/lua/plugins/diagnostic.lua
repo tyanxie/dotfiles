951786b5afe8cc0cadefbd6ce88a5b8a371b4b18
tyanxie <494966054@qq.com>
2025-02-08 13:07:41 +0800

    feat(neovim): 新增rachartier/tiny-inline-diagnostic.nvim插件用于美化诊断信息展示

---
+++ nvim/lua/plugins/diagnostic.lua
@@ -0,0 +1,27 @@
+-- 初始化诊断信息配置
+local icons = require("util.icons")
+vim.diagnostic.config({
+    bufferline = true,
+    float = true,
+    hdlr = false,
+    underline = true,
+    update_in_insert = false,
+    virtual_text = false,
+    severity_sort = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = icons.diagnostics.Error,
+            [vim.diagnostic.severity.WARN] = icons.diagnostics.Warn,
+            [vim.diagnostic.severity.HINT] = icons.diagnostics.Hint,
+            [vim.diagnostic.severity.INFO] = icons.diagnostics.Info,
+        },
+    },
+})
+
+-- 美化诊断信息展示
+return {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    opts = {},
+}
