b5a5597ea919c4ba2705672d9ce572ec0aed1811
tyanxie <494966054@qq.com>
2025-02-08 12:51:08 +0800

    feat(neovim): 修复go.nvim插件覆盖diagnostic配置的问题

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -3,14 +3,14 @@ return {
         "ray-x/go.nvim",
         event = { "CmdlineEnter" },
         ft = { "go", "gomod" },
-        -- build = ':lua require("go.install").update_all_sync()',
         dependencies = {
             "ray-x/guihua.lua",
             "neovim/nvim-lspconfig",
             "nvim-treesitter/nvim-treesitter",
         },
-        config = function()
-            require("go").setup()
-        end,
+        opts = {
+            -- 关闭自动设置vim.diagnostic.config
+            diagnostic = false,
+        },
     },
 }
