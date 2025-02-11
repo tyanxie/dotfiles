797e2026da6c979dde68e7aedca4fced1767841a
tyanxie <494966054@qq.com>
2025-02-11 19:53:20 +0800

    feat(wezterm): 支持手动开关背景图片模式或透明背景模糊模式并新增全屏快捷键

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -31,39 +31,60 @@ config.window_decorations = "TITLE | RESIZE"
 -- 默认光标风格
 config.default_cursor_style = "SteadyBlock"
 
--- macOS下窗口背景模糊
-config.macos_window_background_blur = 70
--- 文字背景透明度
--- 在Neovim中相当于colorscheme相对于wezterm背景图的不透明度
-config.text_background_opacity = 0.8
--- 背景，配置参考：https://www.bilibili.com/video/BV1miWMe9Esq
-config.background = {
-	-- 背景图片
-	{
-		source = {
-			File = wezterm.config_dir .. "/background.jpg",
+-- 是否开启背景图片，为ture的时候使用背景图片配置，false时使用透明背景模糊模式
+-- TODO: 后续可以联动neovim配置做成环境变量进行一键切换
+local enableBackgroundImage = false
+if enableBackgroundImage then
+	-- 使用背景图片模式
+	-- 文字背景透明度
+	-- 在Neovim中相当于colorscheme相对于wezterm背景图的不透明度
+	config.text_background_opacity = 0.8
+	-- 背景图片，配置参考：https://www.bilibili.com/video/BV1miWMe9Esq
+	config.background = {
+		-- 背景图片
+		{
+			source = {
+				File = wezterm.config_dir .. "/background.jpg",
+			},
+			hsb = {
+				-- 色相
+				hue = 1.0,
+				-- 饱和度
+				saturation = 1.02,
+				-- 亮度
+				brightness = 0.25,
+			},
 		},
-		hsb = {
-			-- 色相
-			hue = 1.0,
-			-- 饱和度
-			saturation = 1.02,
-			-- 亮度
-			brightness = 0.25,
+		-- 为背景图片覆盖一层蒙版，防止过亮
+		{
+			source = {
+				Color = "#282c35",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.55,
 		},
-	},
-	-- 为背景图片覆盖一层蒙版，防止过亮
-	{
-		source = {
-			Color = "#282c35",
-		},
-		width = "100%",
-		height = "100%",
-		opacity = 0.55,
-	},
-}
+	}
+else
+	-- 使用透明背景模糊模式
+	-- 窗口背景不透明度
+	config.window_background_opacity = 0.8
+	-- macOS下窗口背景模糊
+	config.macos_window_background_blur = 30
+end
 
 -- 蜂鸣提示音
 config.audible_bell = "Disabled"
+
+-- 快捷键配置
+config.keys = {
+	-- <C-N>键触发全屏
+	-- 配合配置native_macos_fullscreen_mode的默认值false可以做到使用全屏时使用macOS传统的全屏转换，使得全屏时也能看到背景模糊效果
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
 
 return config
