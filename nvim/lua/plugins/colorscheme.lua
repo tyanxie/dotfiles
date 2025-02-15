0dd0d069c4c1c997970ecdacec338a5e7878f077
tyanxie <494966054@qq.com>
2025-02-15 11:46:34 +0800

    feat(neovim.colorscheme): 移除不再使用的colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,43 +1,4 @@
 return {
-    -- tokyonight
-    -- 1. recommend style: light:day, dark:storm
-    -- 2. recommend use with auto-dark-mode to auth change light or dark
-    -- 3. do not disable tokyonight because of this is default installed theme with lazyvim
-    {
-        "folke/tokyonight.nvim",
-        enabled = false,
-        lazy = true,
-        priority = 1000,
-    },
-
-    -- cyberdream
-    {
-        "scottmckendry/cyberdream.nvim",
-        enabled = false,
-        lazy = true,
-        priority = 1000,
-        opts = {
-            transparent = true,
-        },
-    },
-
-    -- nightfox
-    -- recommend light: dayfox
-    {
-        "EdenEast/nightfox.nvim",
-        enabled = false,
-        lazy = false,
-        priority = 1000,
-        opts = {
-            options = {
-                transparent = false,
-                styles = {
-                    comments = "italic",
-                },
-            },
-        },
-    },
-
     {
         "catppuccin/nvim",
         name = "catppuccin",
@@ -88,24 +49,6 @@ return {
                 snacks = true,
                 which_key = true,
             },
-        },
-    },
-
-    -- auto-dark-mode
-    -- auto change dark or light mode
-    {
-        "f-person/auto-dark-mode.nvim",
-        enabled = false,
-        opts = {
-            update_interval = 1000,
-            set_dark_mode = function()
-                vim.api.nvim_set_option_value("background", "dark", {})
-                vim.cmd([[colorscheme nightfox]])
-            end,
-            set_light_mode = function()
-                vim.api.nvim_set_option_value("background", "light", {})
-                vim.cmd([[colorscheme nightfox]])
-            end,
         },
     },
 }
