41a64190c268384e4e3894c811654baa921d6bb2
tyanxie <494966054@qq.com>
2025-02-26 19:57:18 +0800

    feat(neovim.commands): 新增ConvertTimestamp命令，用于将时间戳转换为时间字符串

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -34,3 +34,53 @@ vim.api.nvim_create_user_command("CreateTempFile", function()
 end, {
     desc = "创建临时文件并打开",
 })
+
+-- 时间戳转换为时间字符串，单位s
+vim.api.nvim_create_user_command("ConvertTimestamp", function(opts)
+    -- 按照参数长度获取时间戳文本和单位
+    local text = ""
+    if #opts.fargs == 0 then
+        -- 没有传任何参数时获取光标下的内容作为时间戳文本
+        text = vim.fn.expand("<cword>")
+    elseif #opts.fargs == 1 then
+        -- 只有一个参数时认为第一个参数是时间戳文本
+        text = opts.fargs[1]
+    else
+        vim.notify("参数过多：" .. #opts.fargs, vim.log.levels.ERROR)
+        return
+    end
+
+    -- 时间戳文本转为数字
+    local timestamp = tonumber(text)
+    -- 错误处理
+    if not timestamp or timestamp < 0 then
+        vim.notify("时间戳无效：" .. text, vim.log.levels.ERROR)
+        return
+    end
+
+    -- 解析为时间字符串
+    local result = os.date("%Y-%m-%d %H:%M:%S %z", timestamp)
+    if type(result) ~= "string" then
+        vim.notify("解析结果异常", vim.log.levels.ERROR)
+        return
+    end
+
+    -- 创建浮动窗口
+    Snacks.win({
+        relative = "cursor",
+        width = #result, -- 窗口宽度
+        height = 1, -- 窗口高度
+        row = 1, -- 显示在光标下方
+        col = 0,
+        border = "rounded", -- 边框样式 (可选：single/double/rounded/solid/shadow)
+        title = { { " ", "Title" }, { text } }, -- 标题icon使用Title高亮组，文本为解析使用的时间戳，使用默认高亮组
+        title_pos = "center",
+        text = result,
+        wo = {
+            winhighlight = "Normal:WarningMsg", -- 使用Normal设置窗口文本的高亮组
+        },
+    })
+end, {
+    desc = "时间戳转换为时间字符串",
+    nargs = "*",
+})
