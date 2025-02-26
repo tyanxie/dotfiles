200ba9c053190761f72bf4e1820a58724a69b998
tyanxie <494966054@qq.com>
2025-02-26 10:45:28 +0800

    feat(neovim.surround): 使用echasnovski/mini.surround插件替代kylechui/nvim-surround插件

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -68,4 +68,20 @@ return {
             }
         end,
     },
+
+    -- 向目标内容周围添加surround符号
+    {
+        "echasnovski/mini.surround",
+        opts = {
+            mappings = {
+                add = "Sa", -- Add surrounding in Normal and Visual modes
+                delete = "Sd", -- Delete surrounding
+                find = "Sf", -- Find surrounding (to the right)
+                find_left = "SF", -- Find surrounding (to the left)
+                highlight = "Sh", -- Highlight surrounding
+                replace = "Sr", -- Replace surrounding
+                update_n_lines = "Sn", -- Update `n_lines`
+            },
+        },
+    },
 }
