b5a5ed87cc5890ce127f42aafea5853eb1a48af2
tyanxie <494966054@qq.com>
2025-03-11 21:20:37 +0800

    feat(neovim.mason): 新增默认安装json-to-struct

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -15,6 +15,7 @@ return {
       "gomodifytags", -- golang:github.com/fatih/gomodifytags
       "impl", -- golang:github.com/josharian/impl
       "delve", -- golang:github.com/go-delve/delve/cmd/dlv
+      "json-to-struct", -- golang:github.com/tmc/json-to-struct
       "golangci-lint", -- github:golangci/golangci-lint
       "protols", -- cargo:protols
       "bash-language-server", -- npm:bash-language-server
