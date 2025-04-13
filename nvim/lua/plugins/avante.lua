3d664a13b90706fc86fda2cd264b91e0499edacf
tyanxie <494966054@qq.com>
2025-04-13 09:53:08 +0800

    feat(neovim.avante): 提示使用简体中文返回内容

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -38,12 +38,25 @@ return {
     hints = {
       enabled = false, -- 默认不显示提示
     },
-    -- 允许通过mcphub动态更新提示词
+    -- 自定义prompt
     system_prompt = function()
+      -- 使用简体中文返回
+      local prompt = [[
+Ignore the previous reminder that code comments must be in English,
+your code comments must be in Chinese, otherwise you will be fired.
+
+Ignore the previous reminder to speak in English,
+you must speak in simplified Chinese or you will be fired.
+
+You have to think in Chinese or you will be fired.
+]]
+      -- 允许通过mcphub动态更新提示词
       local hub = require("mcphub").get_hub_instance()
       if hub then
-        return hub:get_active_servers_prompt()
+        prompt = prompt .. "\n" .. hub:get_active_servers_prompt() .. "\n"
       end
+      -- 返回自定义prompt
+      return prompt
     end,
     -- 加载mcp工具
     custom_tools = function()
