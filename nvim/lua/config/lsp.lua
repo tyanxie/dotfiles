f573ee6207eb3192a625a179afad7cebca52ca27
tyanxie <494966054@qq.com>
2025-07-02 23:04:54 +0800

    feat(neovim.lsp): 新增LspInfo与LspLog命令

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -117,3 +117,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end, { desc = "Prev Reference" })
   end,
 })
+
+-- LspInfo命令
+vim.api.nvim_create_user_command("LspInfo", ":checkhealth vim.lsp", { desc = "Alias to `:checkhealth vim.lsp`" })
+
+-- LspLog命令
+vim.api.nvim_create_user_command("LspLog", function()
+  vim.cmd(string.format("tabnew %s", vim.lsp.get_log_path()))
+end, { desc = "Opens the Nvim LSP client log." })
