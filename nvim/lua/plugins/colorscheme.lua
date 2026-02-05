d3b5c011c6d7ad0b373e8fe544a6c66e7cb74793
tyanxie <494966054@qq.com>
2026-02-05 20:58:26 +0800

    feat(neovim.colorscheme): Go语言字符串格式化占位符支持高亮

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -52,6 +52,15 @@ return {
         }
       end,
     },
+    -- 自定义高亮组，可使用的key可以通过光标悬停在要高亮的内容上，然后使用 `:Inspect` 命令查看
+    custom_highlights = function(colors)
+      return {
+        -- Go语言格式化字符串占位符，如fmt.Sprintf中的%s等标识
+        ["@lsp.mod.format.go"] = { fg = colors.teal },
+        -- Go语言更精准的格式化字符串占位符，如fmt.Sprintf中的%s等标识
+        ["@lsp.typemod.string.format.go"] = { fg = colors.teal },
+      }
+    end,
     -- Neovim生态中其它插件的主题支持
     integrations = {
       blink_cmp = true,
